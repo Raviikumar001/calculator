742545dd851728b7934e64c8ffb773377fce4e09
Raviikumar001 <samcollins9899@gmail.com>
2021-11-20 21:57:11 +0530

    added dynamice input

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ let btn4 = document.querySelector("#btn4");
 let msg = document.querySelector("#msg");
 let result = 0;
 // this funciton is to extract values into the first and second variables.
-
+let first=0;
+let second=0;
+let operation = null;
 function numAssign()
 { 
-  let first = Number(num1.value);
-  let second = Number(num2.value);
+   first = Number(num1.value);
+   second = Number(num2.value);
 
 }
 
@@ -24,30 +26,79 @@ function numAssign()
 function start()
 {
   btn1.addEventListener("click", function () {
-   
+   console.log("hi")
     numAssign();
-    result = first + second;
-    msg.innerText = result;
+    
+    operation="+";
+
+    calc();
   });
   
   btn2.addEventListener("click", () => {
     numAssign();
-    result = first - second;
-    msg.innerText = result;
+    
+    operation="-";
+    calc();
   });
   
   btn3.addEventListener("click", () => {
     numAssign();
-    result = first * second;
-    msg.innerText = result;
+    
+    operation="*";
+    calc();
   });
   
   btn4.addEventListener("click", () => {
     numAssign();
-    result = first / second;
-    msg.innerText = result;
+    
+    operation="/";
+    calc();
   });
   
 
 }
 
+start();
+
+function calc(){
+  if(operation==="+")
+  {
+    result=first+second;
+    msg.innerText=result;
+  }
+  if(operation==="-")
+  {
+    result=first-second;
+    msg.innerText=result;
+  }
+  if(operation==="*")
+  {
+    result=first*second;
+    msg.innerText=result;
+  }
+  if(operation==="/")
+  {
+    result=first/second;
+    msg.innerText=result;
+  }
+  
+}
+num1.addEventListener("change",(e)=>{
+  first=Number(e.target.value);
+  if(operation!==null)
+  {
+    calc();
+    }  
+
+
+})
+
+num2.addEventListener("change",(e)=>{
+  second=Number(e.target.value);
+  if(operation!==null)
+  {
+    calc();
+    }  
+
+
+})
